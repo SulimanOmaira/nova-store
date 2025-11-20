d8809347571ca84ebf59e0ef38aae09cb67c40f2
SulimanOmaira <suliman1omira@gmail.com>
2025-11-20 23:41:00 +0300

    Clean untracked files

--- src/a-cod-status/a-cod-status.controller.ts
+++ src/a-cod-status/a-cod-status.controller.ts
@@ -11,13 +11,13 @@ import { RolesGuard } from 'src/auth/roles.guard';
 export class ACodStatusController {
   constructor(private readonly aCodStatusService: ACodStatusService) {}
   
-  // @UseGuards(JwtAuthGuard, RolesGuard)
-  // @Roles(Role.ADMIN)
-  // @Post('create')
-  // create(@Req() req ,@Body() createACodStatusDto: CreateACodStatusDto) {
-  //   const userID = req.user.userId;
-  //   return this.aCodStatusService.create(createACodStatusDto, userID);
-  // }
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Post('create')
+  create(@Req() req ,@Body() createACodStatusDto: CreateACodStatusDto) {
+    const userID = req.user.userId;
+    return this.aCodStatusService.create(createACodStatusDto, userID);
+  }
 
   @Get('all')
   findAll() {
